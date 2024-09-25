f8bceddcb561a9aa1ff0180a4942f831edd2492d
nguivo <nguivo9@gmail.com>
2024-09-25 03:03:26 +0100

    working with dates and time in java

---
+++ T007DateTime.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class T007DateTime {
+
+    public static void main(String[] args) {
+        LocalDate myDate = LocalDate.now();
+        LocalTime myTime = LocalTime.now();
+        LocalDateTime dates = LocalDateTime.now();
+
+        DateTimeFormatter myFormats = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
+        String formattedDate = myDate.format(myFormats);
+
+        System.out.println(myDate);
+        System.out.println(myTime);
+        System.out.println(dates);
+        System.out.println(formattedDate);
+    }
+}
